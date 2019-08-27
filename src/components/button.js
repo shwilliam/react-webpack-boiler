1fe71ce4dbb32462b1e0e3279ea44990c405b144
William L <w-lindvall@outlook.com>
2019-08-27 13:07:36 -0700

    prettier

--- src/components/button.js
+++ src/components/button.js
@@ -3,9 +3,7 @@ import React, {useState} from 'react'
 const Button = () => {
   const [clicks, setClicks] = useState(0)
 
-  return (
-    <button onClick={() => setClicks(clicks + 1)}>{clicks}</button>
-  )
+  return <button onClick={() => setClicks(clicks + 1)}>{clicks}</button>
 }
 
 export default Button
